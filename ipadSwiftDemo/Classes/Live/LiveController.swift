a9bdbbe6c307151be54e76291ddb24f4d11b2d90
fantasy <1239002985@qq.com>
2017-03-07 17:50:31 +0800

    can download

--- ipadSwiftDemo/Classes/Live/LiveController.swift
+++ ipadSwiftDemo/Classes/Live/LiveController.swift
@@ -18,8 +18,12 @@ class LiveController: UIViewController {
     var url5 = "http://media7.smartstudy.com/pd/videos/2015/af/c7/16040/mp4/dest.m3u8"
     let url6 = "http://media7.smartstudy.com/pd/videos/2015/3e/5a/16041/mp4/dest.m3u8"
     let url7 = "http://v.smartstudy.com/pd/videos/2015/67/df/10422/mp4/dest.m3u8"
+  let url8 = "http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.2.4.dmg"
 
     var downloadTool: DownloadToolManage!
+  var labelProgress : UILabel!
+  var labelSpeed :UILabel!
+  var labelRemainTime:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,9 +38,9 @@ class LiveController: UIViewController {
 
         let downloadButton = UIButton(type: .custom)
         downloadButton.backgroundColor = UIColor.randomColor()
-        downloadButton.setTitle("下载", for: UIControlState.normal)
+        downloadButton.setTitle("重新下载", for: UIControlState.normal)
         downloadButton.setTitleColor(UIColor.colorWithRGB(red: 33, green: 33, blue: 33), for: UIControlState.normal)
-        downloadButton.addTarget(self, action: #selector(clickToDownlod(button:)), for: UIControlEvents.touchUpInside)
+        downloadButton.addTarget(self, action: #selector(clickToDownload(button:)), for: UIControlEvents.touchUpInside)
         self.view.addSubview(downloadButton)
         downloadButton.snp.makeConstraints { make in
             make.left.top.equalTo(20)
@@ -44,51 +48,85 @@ class LiveController: UIViewController {
             make.height.equalTo(40)
         }
 
-        let deletButton = UIButton(type: .custom)
-        deletButton.backgroundColor = UIColor.randomColor()
-        deletButton.setTitle("删除", for: UIControlState.normal)
-        deletButton.setTitleColor(UIColor.colorWithRGB(red: 33, green: 33, blue: 33), for: UIControlState.normal)
-        deletButton.addTarget(self, action: #selector(clickToDelet(button:)), for: UIControlEvents.touchUpInside)
-        self.view.addSubview(deletButton)
-        deletButton.snp.makeConstraints { make in
+        let goonButton = UIButton(type: .custom)
+        goonButton.backgroundColor = UIColor.randomColor()
+        goonButton.setTitle("继续下载", for: UIControlState.normal)
+        goonButton.setTitleColor(UIColor.colorWithRGB(red: 33, green: 33, blue: 33), for: UIControlState.normal)
+        goonButton.addTarget(self, action: #selector(clickToGoon(button:)), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(goonButton)
+        goonButton.snp.makeConstraints { make in
             make.left.equalTo(downloadButton.snp.right).offset(20)
             make.top.equalTo(20)
             make.width.equalTo(100)
             make.height.equalTo(40)
         }
+      
+      let pauseButton = UIButton(type: .custom)
+      pauseButton.backgroundColor = UIColor.randomColor()
+      pauseButton.setTitle("暂停", for: UIControlState.normal)
+      pauseButton.setTitleColor(UIColor.colorWithRGB(red: 33, green: 33, blue: 33), for: UIControlState.normal)
+      pauseButton.addTarget(self, action: #selector(clickToPause(button:)), for: UIControlEvents.touchUpInside)
+      self.view.addSubview(pauseButton)
+      pauseButton.snp.makeConstraints { make in
+        make.left.equalTo(20)
+        make.top.equalTo(downloadButton.snp.bottom).offset(20)
+        make.width.equalTo(100)
+        make.height.equalTo(40)
+      }
+      
+      let cancelButton = UIButton(type: .custom)
+      cancelButton.backgroundColor = UIColor.randomColor()
+      cancelButton.setTitle("取消下载", for: UIControlState.normal)
+      cancelButton.setTitleColor(UIColor.colorWithRGB(red: 33, green: 33, blue: 33), for: UIControlState.normal)
+      cancelButton.addTarget(self, action: #selector(clickToCancel(button:)), for: UIControlEvents.touchUpInside)
+      self.view.addSubview(cancelButton)
+      cancelButton.snp.makeConstraints { make in
+        make.left.equalTo(pauseButton.snp.right).offset(20)
+        make.top.equalTo(goonButton.snp.bottom).offset(20)
+        make.width.equalTo(100)
+        make.height.equalTo(40)
+      }
+      
+      labelSpeed = self.setupLabel()
+      labelSpeed.snp.makeConstraints { (make) in
+        make.left.equalTo(20)
+        make.top.equalTo(pauseButton.snp.bottom).offset(20)
+      }
+      
+      labelProgress = self.setupLabel()
+      labelProgress.snp.makeConstraints { (make) in
+        make.left.equalTo(20)
+        make.top.equalTo(labelSpeed.snp.bottom).offset(20)
+      }
+      labelRemainTime = self.setupLabel()
+      labelRemainTime.snp.makeConstraints { (make) in
+        make.left.equalTo(20)
+        make.top.equalTo(labelProgress.snp.bottom).offset(20)
+      }
+      
     }
+  func clickToCancel(button _:UIButton) {
+    print("clickToCancel")
+    self.downloadTool.cancelDownload()
+  }
+  func clickToGoon(button _: UIButton) {
+    print("clickToGoon")
+    self.downloadTool.goonDownload()
+  }
+  func clickToPause(button _: UIButton) {
 
-    func clickToDelet(button _: UIButton) {
+    print("clickToPause")
+    self.downloadTool.pauseDownload()
+  }
 
-        print("clickToDelet")
-    }
-
-    func changeUrlStringToName(urlString: String) -> String {
-
-        var name = urlString.subStringTo(to: (urlString.length - 14))
-        if name.hasPrefix("http://") {
-            name = name.SubStringFrom(from: 7)
-        }
-
-        let newName = name.replacingOccurrences(of: "/", with: "~") + ".mp4"
-
-        print("name == \(name)")
-        print("newName ==\(newName)")
-        return newName
-    }
-
-    func clickToDownlod(button _: UIButton) {
+    func clickToDownload(button _: UIButton) {
         print("clickToDownlod")
 
-        let startBlock = { (url: String, totalSize: String) in
-            print("startBlock url ==\(url)")
-            print("startBlock totalSize==\(totalSize)")
-        }
         let progressBlock = { (progress: String, remainTime: String, speed: String) in
             print("progressBlock")
-            print("progress ==\(progress)")
-            print("remainTime ==\(remainTime)")
-            print("speed ==\(speed)")
+          self.labelRemainTime.text = "剩余时间:" + remainTime
+          self.labelSpeed.text = "速度:" + speed
+          self.labelProgress.text = "进度:" + progress
         }
         let completeBlock = { (path: String) in
             print("completeBlock path ==\(path)")
@@ -97,67 +135,28 @@ class LiveController: UIViewController {
             print("failedBlock error==", error)
         }
         downloadTool = DownloadToolManage()
-        downloadTool.downloadVideoFiles(downloadUrl: url5, toSavePath: "asd", startBlock: startBlock, progressBlock: progressBlock, completeBlock: completeBlock, failedBlock: failedBlock)
+        downloadTool.downloadVideoFiles(downloadUrl: url8, toSavePath: "asd", progressBlock: progressBlock, completeBlock: completeBlock, failedBlock: failedBlock)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  func setupLabel() -> UILabel {
+    let label = UILabel.init()
+    label.textColor = UIColor.colorWithHexString("333333")
+    label.font = UIFont.systemFont(ofSize: 15)
+    label.backgroundColor = UIColor.brown
+    self.view.addSubview(label)
+    return label
+  }
+  
+  func changeUrlStringToName(urlString: String) -> String {
+    
+    var name = urlString.subStringTo(to: (urlString.length - 14))
+    if name.hasPrefix("http://") {
+      name = name.SubStringFrom(from: 7)
     }
-
-    /*
-     点击下载
-     let url = URL(string: url5)
-
-     //是否存在这个url
-     if(VeloxCacheManagement.fileExistForURL(url: url!)){
-     print("这个url 已经下载好了")
-     return
-     } else {
-     print("不存在这个url")
-     }
-
-     let veloxDownloader = VeloxDownloadManager.sharedInstance
-     let progressClosure : (CGFloat,VeloxDownloadInstance) -> (Void)
-     let remainingTimeClosure : (CGFloat) -> Void
-     let completionClosure : (Bool) -> Void
-
-     progressClosure = {(progress,downloadInstace) in
-     print("Progress of File : \(downloadInstace.filename) is \(Float(progress))")
-     }
-
-     remainingTimeClosure = {(timeRemaning) in
-     print("Remaining Time is : \(timeRemaning)")
-     }
-
-     completionClosure = {(status) in
-     print("is Download completed : \(status)")
-     }
-
-     let name = self.changeUrlStringToName(urlString: url5)
-
-     veloxDownloader.downloadFile(
-     withURL: url!,
-     name: name,
-     directoryName: nil,
-     friendlyName: nil,
-     progressClosure: progressClosure,
-     remainigtTimeClosure: remainingTimeClosure,
-     completionClosure: completionClosure,
-     backgroundingMode: false
-     )
-
-     */
-
-    /*
-     print("clickToDelet")
-     let url = URL(string: url5)
-     if(VeloxCacheManagement.fileExistForURL(url: url!)){
-     let result =  VeloxCacheManagement.deleteFileForURL(url: url!)
-     print("deleteFileForURL : \(result)")
-     } else {
-     print("不存在这个url")
-     }
-
-     */
+    
+    let newName = name.replacingOccurrences(of: "/", with: "~") + ".mp4"
+    
+    print("name == \(name)")
+    print("newName ==\(newName)")
+    return newName
+  }
 }
--- ipadSwiftDemo/Common/Tools/DownloadTool/DownloadToolManage.swift
+++ ipadSwiftDemo/Common/Tools/DownloadTool/DownloadToolManage.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class DownloadToolManage: NSObject, URLSessionDelegate {
+class DownloadToolManage: NSObject,URLSessionDownloadDelegate {
 
     // 进度的String 22.0%  剩余时间的String 04:46 速度的String 300KB/s
     typealias DownloadingProgress = (String, String, String) -> Void
     // 返回的path
     typealias DownloadedComplete = (String) -> Void
-    // 开始下载url 和文件的总大小
-    typealias DownloadStart = (String, String) -> Void
     // 下载失败
     typealias DownloadedFailed = (Error) -> Void
 
-    public var startBlock: DownloadStart?
     public var progressBlock: DownloadingProgress?
     public var completeBlock: DownloadedComplete?
     public var failed: DownloadedFailed?
@@ -27,6 +24,8 @@ class DownloadToolManage: NSObject, URLSessionDelegate {
     var downloadUrl: String?
     var toSavePath: String?
     var session: URLSession?
+    var downloadTask :URLSessionDownloadTask?
+    var downloadDate : Date?
 
     override init() {
         super.init()
@@ -38,7 +37,6 @@ class DownloadToolManage: NSObject, URLSessionDelegate {
     public func downloadVideoFiles(
         downloadUrl: String,
         toSavePath: String,
-        startBlock: @escaping DownloadStart,
         progressBlock: @escaping DownloadingProgress,
         completeBlock: @escaping DownloadedComplete,
         failedBlock: @escaping DownloadedFailed
@@ -48,13 +46,14 @@ class DownloadToolManage: NSObject, URLSessionDelegate {
         assert(downloadUrl.length > 0, "this url should not be nil")
         self.downloadUrl = downloadUrl
         self.toSavePath = toSavePath
-        self.startBlock = startBlock
         self.progressBlock = progressBlock
         self.completeBlock = completeBlock
         self.failed = failedBlock
+        self.downloadDate = Date()
 
         startDownload()
     }
+  
 
     func startDownload() {
         print("this func is startDownload")
@@ -62,29 +61,59 @@ class DownloadToolManage: NSObject, URLSessionDelegate {
 
         // 开始下载
         let request = URLRequest(url: URL(string: self.downloadUrl!)!)
-        let downloadTask = self.session?.downloadTask(with: request)
+        downloadTask = self.session?.downloadTask(with: request)
         downloadTask?.resume()
-        if self.startBlock != nil {
-            self.startBlock!(self.downloadUrl!, "10M")
-        }
+  
+    }
+  //暂停
+  func pauseDownload() {
+    self.downloadTask?.suspend()
+  }
+  //继续
+  func goonDownload() {
+    self.downloadTask?.resume()
+  }
+  //取消
+  func cancelDownload() {
+    self.downloadTask?.cancel()
+  }
+  
+  //下载出错
+  func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+    print("error ==",error)
+  }
+  
+  //下载完成
+  func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+    print("location ==",location)
+  }
+  //监听下载进度的方法
+  func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+    
+    let  progress = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+    var progressString:String
+    if progress == 1 {
+      progressString = "100%"
+    } else {
+      progressString = String(format:"%.2f",progress*100) + "%"
+    }
+    
+    let timeInterval : TimeInterval  = Date().timeIntervalSince(self.downloadDate!)
+    let speed  = CGFloat.init(totalBytesWritten) / CGFloat(timeInterval)
+    print("speed ==",speed)
+    
+    let remainingBytes = totalBytesExpectedToWrite - totalBytesWritten
+    let remainingTime = CGFloat(remainingBytes) / CGFloat(speed)
+    print("remainingTime ==",remainingTime)
+    
+    DispatchQueue.main.async {
+      
+      if self.progressBlock != nil {
+        self.progressBlock!(progressString,"\(remainingTime)","\(speed)")
+      }
+      
     }
 
-    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler _: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-
-        print("session ==\(session)")
-        print("challenge ==\(challenge)")
-        //    print("AuthChallengeDisposition",URLSession.AuthChallengeDisposition())
-        print("URLCredential", URLCredential())
-    }
-
-    func urlSessionDidFinishEvents(forBackgroundURLSession _: URLSession) {
-
-        print("urlSessionDidFinishEvents")
-    }
-
-    func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {
-
-        print("session == ", session)
-        print("error ==", error)
-    }
+  }
+ 
 }
