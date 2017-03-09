dd57c4b3311a64a7a2e74e1f74fb5ac4fe1da676
fantasy <1239002985@qq.com>
2017-03-09 10:30:18 +0800

    add curve view

--- ipadSwiftDemo/Classes/Home/HomeAllController.swift
+++ ipadSwiftDemo/Classes/Home/HomeAllController.swift
@@ -12,6 +12,7 @@ class HomeAllController: UIViewController,UITableViewDataSource,UITableViewDeleg
   
   var dataSourceArray:Array<String>!
   var tableView:UITableView!
+  var curve:CurveView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,14 @@ class HomeAllController: UIViewController,UITableViewDataSource,UITableViewDeleg
       
     }
   func initData() {
+    
+    curve = CurveView.init(frame: .zero)
+    self.view.addSubview(curve)
+    curve.snp.makeConstraints { (make) in
+      make.top.right.left.equalTo(0)
+      make.height.equalTo(curveMaxHeight/2)
+    }
+    
     dataSourceArray = [
       "发如雪",
       "东风破",
@@ -39,6 +48,11 @@ class HomeAllController: UIViewController,UITableViewDataSource,UITableViewDeleg
       "白色风车",
       "晴天",
       "彩虹",
+      "稻香",
+      "枫",
+      "鞋子特大号",
+      "借口",
+      "暗号",
     ]
     
   }
@@ -51,9 +65,38 @@ class HomeAllController: UIViewController,UITableViewDataSource,UITableViewDeleg
     tableView.separatorStyle = .none
     self.view.addSubview(tableView)
     tableView.snp.makeConstraints { (make) in
-      make.top.left.right.bottom.equalTo(0)
+      make.left.right.bottom.equalTo(0)
+      make.top.equalTo(curve.snp.bottom)
     }
     
+    tableView.addObserver(self, forKeyPath: "contentOffset", options: .new, context: nil)
+    
+  }
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    
+    if keyPath == "contentOffset" {
+      
+      let currentPoint = self.tableView.contentOffset
+      
+      var currentY:CGFloat = currentPoint.y
+      if  currentY > curveMaxHeight{
+        currentY = curveMaxHeight
+      } else if (currentY < 0) {
+        currentY = 0
+      }
+      currentY = curveMaxHeight - currentY
+      
+      self.curve.updateMaxHeight(thisHeight: currentY)
+      curve.snp.remakeConstraints({ (make) in
+        make.top.left.right.equalTo(0)
+        make.height.equalTo(currentY/2)
+      })
+      
+    } else {
+      
+      super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+    
+    }
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -69,9 +112,9 @@ class HomeAllController: UIViewController,UITableViewDataSource,UITableViewDeleg
     
   }
   
+  deinit {
+    self.tableView.removeObserver(self, forKeyPath: "contentOffset")
+  }
   
-  
-
-
   
 }
